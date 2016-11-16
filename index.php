0560ead1cc227a1669b04c418f53e0e3a7de2729
Peter Snoek <petersnoek@davinci.nl>
2016-11-16 09:51:10 +0100

    configured composer to install standalone laravel blade and created hello world

---
+++ index.php
@@ -0,0 +1,11 @@
+<?php
+
+require 'vendor/autoload.php';
+
+use Philo\Blade\Blade;
+
+$views = __DIR__ . '/views';
+$cache = __DIR__ . '/cache';
+
+$blade = new Blade($views, $cache);
+echo $blade->view()->make('index')->render();
---
+++ views/index.blade.php
@@ -0,0 +1,10 @@
+<!doctype html>
+<html>
+<head>
+    <title>Hello World</title>
+</head>
+<body>
+    <h1>Hallo blije wereld.</h1>
+
+</body>
+</html>
