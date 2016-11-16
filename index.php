6f8f871d2b48a074d48d2f3de3fc4a1db4f9d71b
Peter Snoek <petersnoek@davinci.nl>
2016-11-16 10:01:21 +0100

    show the HTML code that Curl downloaded

--- index.php
+++ index.php
@@ -1,11 +1,15 @@
 <?php
 
 require 'vendor/autoload.php';
-
 use Philo\Blade\Blade;
-
 $views = __DIR__ . '/views';
 $cache = __DIR__ . '/cache';
-
 $blade = new Blade($views, $cache);
-echo $blade->view()->make('index')->render();
+
+// load an html page
+$url = "https://github.com/petersnoek";
+$curl = curl_init($url);
+curl_setopt($curl, CURLOPT_RETURNTRANSFER, TRUE);
+$html = curl_exec($curl);
+
+echo $blade->view()->make('index')->with('html', $html)->render();
--- views/index.blade.php
+++ views/index.blade.php
@@ -1,10 +1,1 @@
-<!doctype html>
-<html>
-<head>
-    <title>Hello World</title>
-</head>
-<body>
-    <h1>Hallo blije wereld.</h1>
-
-</body>
-</html>
+{{ $html }}
